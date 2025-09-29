27fe34431ec13221e32ba8921e52cbb7af7584de
Hmazah <alhamza2012@gmail.com>
2025-09-29 10:00:00 +0300

    instll nodemon

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+console.log(process.env.PORT);
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
