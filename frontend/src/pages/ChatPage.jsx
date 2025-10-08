0a34892760a5261d85c8b5ab40b6b771c9af1e25
Hmazah <alhamza2012@gmail.com>
2025-10-08 22:11:40 +0300

    start building chatpage ui

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -25,9 +25,9 @@ function ChatPage() {
         </div>
 
         {/* RIGHT SIDE */}
-        {/* <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
           {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
-        </div> */}
+        </div>
       </BorderAnimatedContainer>
     </div>
   );
